8fe17dcd3979f033e0e89bdf3ceec72947c7d283
czflem <41273032+czflem@users.noreply.github.com>
2019-04-21 15:17:13 +1000

    Creating log file and append options

--- app/src/main/java/com/example/inspect/LogManager.java
+++ app/src/main/java/com/example/inspect/LogManager.java
@@ -2,13 +2,16 @@ package com.example.inspect;
 
 public class LogManager {
 
+    private File logFile = new File("log.txt");
+
+
     //Allows the classes to report an error//
-    public static void reportError(){
+    public static void reportError(String reportError){
 
     }
 
     //Reports the status of the error//
-    public static void reportStatus(){
+    public static void reportStatus(String reportStatus){
 
     }
 
@@ -19,16 +22,27 @@ public class LogManager {
 
     //Appends or adds errors to the error log//
     public static void appendLog(){
-
+        try{
+            //BufferedWriter for performance, true to set append to file flag
+            BufferedWriter bw = new BufferedWriter(new FileWriter(logFile, true));
+            //variable "text" is a string to be appended
+            bw.append(text);
+            bw.newLine();
+            bw.close();
+        }
+        catch (IOException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     //Displays the error//
-    public static void displayError(){
+    public static void displayError(String displayError){
 
     }
 
     //Displays the status of the error//
-    public static void displayStatus(){
+    public static void displayStatus(String displayStatus){
 
     }
 
