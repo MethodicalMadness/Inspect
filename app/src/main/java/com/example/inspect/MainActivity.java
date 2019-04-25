26c6f55a86943d851c62e4b4c9edca0a6627fd83
MethodicalMadness <lucifersbeard@hotmail.com>
2019-04-26 01:52:35 +1000

    Created a new (blank) template activity Created a simple text field (view) Implemented add text field Adapted pdf export functionality to account for layout changes in template. Implemented basic GUI to support new functionality.

--- app/src/main/java/com/example/inspect/MainActivity.java
+++ app/src/main/java/com/example/inspect/MainActivity.java
@@ -2,20 +2,21 @@ package com.example.inspect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.print.PrintManager;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void printPDF(View view) {
-        PrintManager printManager = (PrintManager) getSystemService(PRINT_SERVICE);
-        printManager.print("print_any_view_job_name", new ViewPrintAdapter(this, findViewById(R.id.linearLayoutVert)), null);
+    public void newBlankTemplate(View view) {
+        Intent intent = new Intent(this, TemplateEditor.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/inspect/TemplateEditor.java
+++ app/src/main/java/com/example/inspect/TemplateEditor.java
@@ -1,6 +1,43 @@
 package com.example.inspect;
 
-public class TemplateEditor {
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.print.PrintManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+
+public class TemplateEditor extends AppCompatActivity{
+
+    private LinearLayout linearLayoutBody;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.template_editor);
+        linearLayoutBody = findViewById(R.id.linearLayoutBody);
+    }
+
+    // Add the new field at bottom of layout.
+    public void onAddField(View view) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.text_field, null);
+        linearLayoutBody.addView(rowView, linearLayoutBody.getChildCount());
+    }
+
+    // Remove selected view
+    public void onDelete(View view) {
+        linearLayoutBody.removeView((View) view.getParent());
+    }
+
+    //Print the scrollView that holds the linearLayoutBody
+    public void printPDF(View view) {
+        PrintManager printManager = (PrintManager) getSystemService(PRINT_SERVICE);
+        printManager.print("print_any_view_job_name", new ViewPrintAdapter(this, findViewById(R.id.scrollView)), null);
+    }
 
     //Saves template//
     public static void saveTemplate(){
