0bd0bdb3f121f023abf2f293fd99d3b7bda1f138
MethodicalMadness <lucifersbeard@hotmail.com>
2019-04-26 22:14:37 +1000

    Can now add pages to PDF New template pages now at A4 page ratio. PDF export supports multiple pages.

--- app/src/main/java/com/example/inspect/TemplateEditor.java
+++ app/src/main/java/com/example/inspect/TemplateEditor.java
@@ -7,18 +7,27 @@ import android.print.PrintManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
 
 
 public class TemplateEditor extends AppCompatActivity{
 
+    //private LinearLayout parentLinearLayout;
+    private LinearLayout linearLayoutPdf;
     private LinearLayout linearLayoutBody;
+    private ArrayList<View> views  = new ArrayList<>();
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.template_editor);
+        //parentLinearLayout = findViewById(R.id.parent_linear_layout);
+        linearLayoutPdf = findViewById(R.id.linearLayoutPdf);
         linearLayoutBody = findViewById(R.id.linearLayoutBody);
+        views.add(findViewById(R.id.scrollViewPage));
     }
 
     // Add the new field at bottom of layout.
@@ -26,6 +35,18 @@ public class TemplateEditor extends AppCompatActivity{
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View rowView = inflater.inflate(R.layout.text_field, null);
         linearLayoutBody.addView(rowView, linearLayoutBody.getChildCount());
+    }
+
+    // Add the new page under previous.
+    public void onAddPage(View view) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View newView = inflater.inflate(R.layout.new_page, null);
+        //Add ScrollView to the list so we keep track of the pages for printing
+        views.add(newView.findViewById(R.id.scrollViewPage));
+        //Add new page above the 3 buttons
+        linearLayoutPdf.addView(newView, linearLayoutPdf.getChildCount()-3);
+        //focus on new page
+        linearLayoutBody = newView.findViewById(R.id.linearLayoutBody);
     }
 
     // Remove selected view
@@ -36,7 +57,7 @@ public class TemplateEditor extends AppCompatActivity{
     //Print the scrollView that holds the linearLayoutBody
     public void printPDF(View view) {
         PrintManager printManager = (PrintManager) getSystemService(PRINT_SERVICE);
-        printManager.print("print_any_view_job_name", new ViewPrintAdapter(this, findViewById(R.id.scrollView)), null);
+        printManager.print("print_job_name", new ViewPrintAdapter(this, views), null);
     }
 
     //Saves template//
--- app/src/main/java/com/example/inspect/ViewPrintAdapter.java
+++ app/src/main/java/com/example/inspect/ViewPrintAdapter.java
@@ -15,38 +15,34 @@ import android.print.PrintDocumentAdapter;
 import android.print.PrintDocumentInfo;
 import android.print.pdf.PrintedPdfDocument;
 import android.view.View;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ViewPrintAdapter extends PrintDocumentAdapter {
 
     private PrintedPdfDocument printedPdfDocument;
     private Context context;
-    private View view;
+    private ArrayList<View> viewsList;
 
-    public ViewPrintAdapter(Context context, View view) {
+    public ViewPrintAdapter(Context context, ArrayList<View> viewsList) {
         this.context = context;
-        this.view = view;
+        this.viewsList = viewsList;
     }
 
     @Override
     public void onLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes,
                          CancellationSignal cancellationSignal,
                          LayoutResultCallback callback, Bundle extras) {
-
         printedPdfDocument = new PrintedPdfDocument(context, newAttributes);
-
         if (cancellationSignal.isCanceled()) {
             callback.onLayoutCancelled();
             return;
         }
-
         PrintDocumentInfo.Builder builder = new PrintDocumentInfo
                 .Builder("print_output.pdf")
                 .setContentType(PrintDocumentInfo.CONTENT_TYPE_DOCUMENT)
-                .setPageCount(1);
-
+                .setPageCount(viewsList.size()` `);
         PrintDocumentInfo info = builder.build();
         callback.onLayoutFinished(info, true);
     }
@@ -55,30 +51,34 @@ public class ViewPrintAdapter extends PrintDocumentAdapter {
     public void onWrite(PageRange[] pages, ParcelFileDescriptor destination,
                         CancellationSignal cancellationSignal,
                         WriteResultCallback callback) {
-
-        // Start the page
-        PdfDocument.Page page = printedPdfDocument.startPage(0);
-        // Create a bitmap and put it a canvas for the view to draw to. Make it the size of the view
-        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        view.draw(canvas);
-        // create a Rect with the view's dimensions.
-        Rect src = new Rect(0, 0, view.getWidth(), view.getHeight());
-        // get the page canvas and measure it.
-        Canvas pageCanvas = page.getCanvas();
-        float pageWidth = pageCanvas.getWidth();
-        float pageHeight = pageCanvas.getHeight();
-        // how can we fit the Rect src onto this page while maintaining aspect ratio?
-        float scale = Math.min(pageWidth / src.width(), pageHeight / src.height());
-        float left = pageWidth / 2 - src.width() * scale / 2;
-        float top = pageHeight / 2 - src.height() * scale / 2;
-        float right = pageWidth / 2 + src.width() * scale / 2;
-        float bottom = pageHeight / 2 + src.height() * scale / 2;
-        RectF dst = new RectF(left, top, right, bottom);
-
-        pageCanvas.drawBitmap(bitmap, src, dst, null);
-        printedPdfDocument.finishPage(page);
+        for(int i = 0; i < viewsList.size();i++){
+            System.out.println(i + " of " + viewsList.size());
+            // Get current View
+            View view = viewsList.get(i);
+            // Start the page
+            PdfDocument.Page page = printedPdfDocument.startPage(i);
+            // Create a bitmap and put it a canvas for the view to draw to. Make it the size of the view
+            Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                    Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            // create a Rect with the view's dimensions.
+            Rect src = new Rect(0, 0, view.getWidth(), view.getHeight());
+            // get the page canvas and measure it.
+            Canvas pageCanvas = page.getCanvas();
+            float pageWidth = pageCanvas.getWidth();
+            float pageHeight = pageCanvas.getHeight();
+            // how can we fit the Rect src onto this page while maintaining aspect ratio?
+            float scale = Math.min(pageWidth / src.width(), pageHeight / src.height());
+            float left = pageWidth / 2 - src.width() * scale / 2;
+            float top = pageHeight / 2 - src.height() * scale / 2;
+            float right = pageWidth / 2 + src.width() * scale / 2;
+            float bottom = pageHeight / 2 + src.height() * scale / 2;
+            RectF dst = new RectF(left, top, right, bottom);
+            //draw page
+            pageCanvas.drawBitmap(bitmap, src, dst, null);
+            printedPdfDocument.finishPage(page);
+        }
 
         try {
             printedPdfDocument.writeTo(new FileOutputStream(
@@ -90,6 +90,8 @@ public class ViewPrintAdapter extends PrintDocumentAdapter {
             printedPdfDocument.close();
             printedPdfDocument = null;
         }
-        callback.onWriteFinished(new PageRange[]{new PageRange(0, 0)});
+        // signal the print framework the document is complete
+        PageRange[] writtenPages = {PageRange.ALL_PAGES};
+        callback.onWriteFinished(writtenPages);
     }
 }
