f34d746aa79e6166f32bd2193454554a8f6bcca2
czfleming <41273032+czflem@users.noreply.github.com>
2019-04-28 13:23:16 +1000

    ContextManager upload for static context use

--- app/src/main/java/com/example/inspect/TemplateEditor.java
+++ app/src/main/java/com/example/inspect/TemplateEditor.java
@@ -1,10 +1,13 @@
 package com.example.inspect;
 
+import android.content.Context;
+
 public class TemplateEditor {
+    
 
     //Saves template//
     public static void saveTemplate(){
-
+        LogManager.reportStatus(ContextManager.getInstance(this).doSomething(), "TEST", "Test");
     }
 
     //Adds an element to the template//
