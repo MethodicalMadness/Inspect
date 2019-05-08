e580282ec6808371fe5f6e0c5e802a37a6e4cce3
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-05-08 23:10:57 +1000

    Created object to store information on buttons in menu

---
+++ app/src/main/java/com/example/inspect/ModuleRef.java
@@ -0,0 +1,48 @@
+package com.example.inspect;
+
+import android.widget.Button;
+
+public class ModuleRef {
+    String moduleName;
+    String textOnBtn;
+    Button btnReference;
+
+    //constructors
+    public ModuleRef(){
+        moduleName = "";
+        textOnBtn = "Add New Module";
+        btnReference = null;
+    }
+
+    public ModuleRef(String newModuleName, String newText, Button newBtn){
+        moduleName = newModuleName;
+        textOnBtn = newText;
+        btnReference = newBtn;
+    }
+
+    //getters and setters
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getTextOnBtn() {
+        return textOnBtn;
+    }
+
+    public void setTextOnBtn(String textOnBtn) {
+        this.textOnBtn = textOnBtn;
+    }
+
+    public Button getBtnReference() {
+        return btnReference;
+    }
+
+    public void setBtnReference(Button btnReference) {
+        this.btnReference = btnReference;
+    }
+}
--- app/src/main/java/com/example/inspect/TemplateMenu.java
+++ app/src/main/java/com/example/inspect/TemplateMenu.java
@@ -6,14 +6,24 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class TemplateMenu extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.template_menu);
+        //set up back btn
         configureBackBtn();
+        //Create Array to display buttons
+        ArrayList<ModuleRef> buttonList = new ArrayList<ModuleRef>();
+        //Add button to list
+        Button btnName = (Button) findViewById(R.id.btnAddName);
+        buttonList.add(new ModuleRef("Template Name", "Name Template", btnName));
+        btnName.setText(buttonList.get(0).getTextOnBtn());
     }
 
+    //Functionality for back button
     public void configureBackBtn(){
         Button backBtn = (Button)findViewById(R.id.btnBack);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +33,6 @@ public class TemplateMenu extends AppCompatActivity {
             }
         });
     }
+
 
 }
