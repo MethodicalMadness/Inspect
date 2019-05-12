a55dc23ee3e57d2dca4c3644f12d15945e80088c
MethodicalMadness <lucifersbeard@hotmail.com>
2019-05-12 22:41:19 +1000

    saving now has file out functionality

--- app/src/main/java/com/example/inspect/Template.java
+++ app/src/main/java/com/example/inspect/Template.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -37,7 +40,7 @@ public class Template {
     }
 
     //Saves current state of the template//
-    public void saveState() {
+    public void saveState(String filename) {
         Context context = App.getContext();
         LogManager.reportStatus(context, "TEMPLATE", "Template");
         String newline = System.getProperty("line.separator");
@@ -53,12 +56,21 @@ public class Template {
                 blueprint += elements.get(ii).deconstructElement() + newline;
             }
         }
-        // TODO: write to file
+        // write to file
+        try {
+            FileOutputStream fOut = new FileOutputStream(filename);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(blueprint);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Loads data from file and creates data objects related
     // to pages and elements that make up templates
-    // TODO: instantiate and bind xml layouts related to data objects
+    // TODO: instantiate and bind xml layouts related to data objects, prob from template editor or inspector as they will have that functionality
     public void loadState(String filename) {
         try (Scanner scanner = new Scanner(new File(filename))) {
             Pattern newPage = Pattern.compile("[0-9]");
