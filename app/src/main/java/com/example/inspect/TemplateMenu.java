49a891c1e0524e7e41b8ccb082f8c817f0fb1cb2
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-05-12 21:54:47 +1000

    Attempt 2, cleaner code, not really

--- app/src/main/java/com/example/inspect/TemplateMenu.java
+++ app/src/main/java/com/example/inspect/TemplateMenu.java
@@ -1,22 +1,24 @@
 package com.example.inspect;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.fragment.app.Fragment;
 
-import java.lang.reflect.Array;
+import android.content.Context;
 import java.util.ArrayList;
 
 public class TemplateMenu extends AppCompatActivity {
-    //variables
-    int index = 0;
 
-    //Create Array to display buttons
-    ArrayList<TemplateButtons> buttonList = new ArrayList<TemplateButtons>();
+    ArrayList<ModuleInfo> moduleInfoList = new ArrayList<ModuleInfo>();
+    int index = 0;
+    ArrayList<View> views = new ArrayList<>();
+    private ConstraintLayout fragContainer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,17 +26,18 @@ public class TemplateMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.template_menu);
 
-        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.newTemplateLayout);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.templateContainer);
 
         //set up back btn
         configureBackBtn();
 
-        //Add button to list
+        //configure button
         Button btnName = (Button) findViewById(R.id.btnAddName);
-        buttonList.add(new TemplateButtons("Template Name", "Name Template", btnName, index));
-        Button newBtn = new Button(this);
-        setUpListeners(index, newBtn);
-        index++;
+        moduleInfoList.add(new ModuleInfo("Name Template",index));
+        btnName.setText(moduleInfoList.get(index).getModuleName());
+
+        fragContainer = (ConstraintLayout) findViewById(R.id.fragContainer);
+
     }
 
     //Functionality for back button
@@ -48,51 +51,28 @@ public class TemplateMenu extends AppCompatActivity {
         });
     }
 
-    //Create onClickListeners to add another button when current is clicked
-    public void setUpListeners(final int index, final Button currentBtn){
-        /*final int currentIndex = index;
-        //Create an onClickListener for button at this index
-        //eventually set this code up to only occur is input was received from next view
+    public void onAddField(View v) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View moduleButton = inflater.inflate(R.layout.fragment_template_button, null);
 
-        Button currentBtn = buttonList.get(currentIndex).getBtnReference();
-        currentBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //Don't think this is right
-                int newIndex = currentIndex + 1;
-                Button newBtn = new Button(null);
-                buttonList.add(new TemplateButtons("Add New Module", "Add New Module", newBtn, newIndex));
-                addButton(newIndex);
-            }
-        });*/
 
-        final int newIndex = index + 1;
-
-        buttonList.get(index).getBtnReference().setOnClickListener(new View.OnClickListener() {
-            public void onClick(View layout) {
-                Button newBtn = currentBtn;
-                buttonList.add(new TemplateButtons("Add New Module", "Add New Module", newBtn, newIndex));
-                addButton(newIndex);
-            }
-        });
-    }
-
-    public void addButton(int newIndex){
         //Constraint Layout
-        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.newTemplateLayout);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.fragContainer);
         ConstraintSet set = new ConstraintSet();
         set.clone(layout);
-        //add button to view
-        Button currentBtn = buttonList.get(newIndex).getBtnReference();
-        int oldIndex = newIndex - 1;
-        Button previousBtn = buttonList.get(oldIndex).getBtnReference();
-        layout.addView(currentBtn);
 
-        set.connect(currentBtn.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 16);
-        set.connect(currentBtn.getId(),ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT, 16);
-        set.connect(currentBtn.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,16);
-        set.constrainHeight(currentBtn.getId(), ConstraintSet.WRAP_CONTENT);
+        fragContainer.addView(moduleButton);
+
+        set.connect(fragContainer.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 16);
+        set.connect(fragContainer.getId(),ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT, 16);
+        set.connect(fragContainer.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,16);
+        set.constrainHeight(fragContainer.getId(), ConstraintSet.WRAP_CONTENT);
         set.applyTo(layout);
-
     }
+
+    public void onDelete(View v) {
+        fragContainer.removeView((View) v.getParent());
+    }
+
 
 }
