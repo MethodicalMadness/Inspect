474e0df2224c8a93cd504766a4833e5078ffeeb6
Elias Zanbaka <e.z100@hotmail.com>
2019-05-26 23:35:26 +1000

    Implemented takePhoto in PhotoManager. Created photo_manager .xml file containing the button to "Open Camera."

--- app/src/main/java/com/example/inspect/PhotoManager.java
+++ app/src/main/java/com/example/inspect/PhotoManager.java
@@ -1,14 +1,50 @@
 package com.example.inspect;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class PhotoManager {
+import androidx.appcompat.app.AppCompatActivity;
 
-    //Allows you to take a Photo to attach to the template//
-    public static void takePhoto(){
+
+public class PhotoManager extends AppCompatActivity {
+
+    ImageView imageView;
+
+    //Allows you to take a photo with the camera//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_manager);
         Context context = App.getContext();
         LogManager.reportStatus(context, "PHOTOMANAGER", "takePhoto");
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+
     }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
 
     //Allows you to alter/edit the photo//
     public static void alterPhoto() {
@@ -21,6 +57,8 @@ public class PhotoManager {
         Context context = App.getContext();
         LogManager.reportStatus(context, "PHOTOMANAGER", "getPhotoFromGallery");
     }
+
+
 
 
 }
