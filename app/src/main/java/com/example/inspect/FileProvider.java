dcfcffbb33448617d83223ec48be8dd9dd933f95
Elias Zanbaka <e.z100@hotmail.com>
2019-05-31 20:07:55 +1000

    Implemented methods createImageFile and dispatchTakePictureIntent which allow the user to save a full-size photo. Implemented the galleryAddPic method to allow the user to add a photo to the gallery. Implemented the setPic method which allows the user to decode a scaled image.

--- app/src/main/java/com/example/inspect/FileProvider.java
+++ app/src/main/java/com/example/inspect/FileProvider.java
@@ -1,4 +1,15 @@
 package com.example.inspect;
 
+import android.net.Uri;
+
+import java.io.File;
+
 public class FileProvider {
+
+
+    private static Uri uri;
+
+    public static Uri getUriForFile(PhotoManager photoManager, String s, File photoFile) {
+        return uri;
+    }
 }
--- app/src/main/java/com/example/inspect/PhotoManager.java
+++ app/src/main/java/com/example/inspect/PhotoManager.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 
 public class PhotoManager extends AppCompatActivity {
@@ -19,6 +26,9 @@ public class PhotoManager extends AppCompatActivity {
     private static final int PICK_IMAGE = 100;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Uri imageUri;
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
 
     //Allows you to take a photo with the camera or get a photo from the image gallery//
     @Override
@@ -36,7 +46,7 @@ public class PhotoManager extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openCamera();
+                dispatchTakePictureIntent();
             }
         });
 
@@ -71,11 +81,85 @@ public class PhotoManager extends AppCompatActivity {
         LogManager.reportStatus(context, "PHOTOMANAGER", "getPhotoFromGallery");
     }
 
+    //Allows you to open the camera and take a photo//
     private void openCamera(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
         Context context = App.getContext();
         LogManager.reportStatus(context, "PHOTOMANAGER", "getPhotoFromCamera");
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    //Add photo to gallery//
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    //Decode a scaled image//
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.Options boptions = null;
+        BitmapFactory.decodeFile(currentPhotoPath, boptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, boptions);
+        imageView.setImageBitmap(bitmap);
     }
 
     //Allows you to alter/edit the photo//
