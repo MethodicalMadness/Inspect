b0eb1609550fcadb7dd4856ccc6e1746b4c00312
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-06-01 20:39:35 +1000

    Attempt at making database work

--- app/src/main/java/com/example/inspect/Data/Template.java
+++ app/src/main/java/com/example/inspect/Data/Template.java
@@ -2,6 +2,7 @@ package com.example.inspect.Data;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
 
 import java.util.ArrayList;
 
@@ -10,5 +11,31 @@ public class Template {
     @PrimaryKey
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Modules> getTemplateModules() {
+        return templateModules;
+    }
+
+    public void setTemplateModules(ArrayList<Modules> templateModules) {
+        this.templateModules = templateModules;
+    }
+
+    @TypeConverters(ModuleTypeConverter.class)
     private ArrayList<Modules> templateModules;
 }
