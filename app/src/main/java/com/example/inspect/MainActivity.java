10189e727a1252534846f4041b30220ce51222af
czfleming <41273032+czflem@users.noreply.github.com>
2019-06-11 18:37:11 +1000

    Load saved template
    Forgot to commit this. Should all be there to work with loading a saved template.

--- app/src/main/java/com/example/inspect/MainActivity.java
+++ app/src/main/java/com/example/inspect/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
@@ -28,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         checkPermissions();
         Context context = App.getContext();
         LogManager.reportStatus(context, "MAINACTIVITY", "onCreate");
-
     }
 
     //setting up menu activities
