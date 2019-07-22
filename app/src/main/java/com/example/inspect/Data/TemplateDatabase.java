d493f92414c15a4bca46a7e4d9bf0b66d07f05e5
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-07-22 23:13:09 +1000

    Remove references to removed dao

--- app/src/main/java/com/example/inspect/Data/TemplateDatabase.java
+++ app/src/main/java/com/example/inspect/Data/TemplateDatabase.java
@@ -4,16 +4,12 @@ package com.example.inspect.Data;
         import androidx.room.RoomDatabase;
         import androidx.room.TypeConverters;
 
-@Database(entities = {Template.class, Template_Module.class, Modules.class, Images.class, Options.class, TextBoxes.class}, version = 1, exportSchema = true)
+@Database(entities = {Template.class,  Modules.class, }, version = 1, exportSchema = true)
 @TypeConverters(UriConverter.class)
 public abstract class TemplateDatabase extends RoomDatabase {
     //set up data access objects
     abstract TemplateDao templateDao();
-    abstract TemplateModulesDao templateModulesDao();
     abstract ModulesDao modulesDao();
-    abstract ImagesDao imagesDao();
-    abstract OptionsDao optionsDao();
-    abstract TextBoxesDao textBoxesDao();
 
     private static volatile TemplateDatabase INSTANCE;
 
